e798d38b30889dbf5c2e6794b0c5bdb60949fe97
Brayan Ronaldo Mojica Barrios <Ronaldo.MojicaBarrios@wnco.com>
2019-02-13 09:35:05 -0500

    Configuring apollo client on client app

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { ApolloProvider} from 'react-apollo';
+import ApolloClient from 'apollo-boost';
 import './App.css';
+const client = new ApolloClient({
+  uri: "http://react-app.test:4000/graphql",
+  onError: ({networkError, graphQLErrors}) => {
+    console.log('graphQLErrors',graphQLErrors);
+    console.log('networkError',networkError);
+  }
+});
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <ApolloProvider client={client}>
+          <h1>Hola</h1>
+        </ApolloProvider>
       </div>
     );
   }
