49c752cb500f3dd2c7ec6f2e2b968300255a06ce
Brayan Ronaldo Mojica Barrios <Ronaldo.MojicaBarrios@wnco.com>
2019-02-15 09:43:01 -0500

    Creat Client with several emails

--- client/src/components/NuevoCliente.js
+++ client/src/components/NuevoCliente.js
@@ -11,22 +11,70 @@ class NuevoCliente extends Component {
             edad: '',
             email: '',
             tipo: '' 
-        }
+        },
+        error: false,
+        emails: []
+    }
+
+    nuevoCampo = () => {
+        this.setState({
+            emails: this.state.emails.concat([{email: ''}])
+        })
+    }
+
+    quitarCampo = i => () => {
+        this.setState({
+            emails: this.state.emails.filter((email, index) => i !== index)
+        })
+    }
+
+    leerCampo = i => e => {
+        const nuevoEmail = this.state.emails.map((email, index) => {
+            if (i !== index) return email;
+            return {
+                ...email,
+                email: e.target.value
+            }
+        });
+
+        this.setState({
+            emails: nuevoEmail
+        });
     }
 
     render() {
+        const { error } = this.state;
+        let respuesta = (error) ? <p className="alert alert-danger p-3 text-center">Todos los cambos son Obligatorios</p> : '';
+
         return (
             <Fragment>
                 <h2 className="text-center">Nuevo Cliente</h2>
+                { respuesta }
                 <div className="row justify-content-center">
-                    <Mutation mutation={NUEVO_CLIENTE}>
+                    <Mutation 
+                        mutation={NUEVO_CLIENTE}
+                        onCompleted={ () =>  this.props.history.push('/')}
+                    >
                     { crearCliente => 
                         <form 
                             className="col-md-8 m-3"
                             onSubmit={ e => {
                                 e.preventDefault();
 
-                                const {nombre, apellido, empresa, edad, tipo, email} = this.state.cliente;
+                                const {nombre, apellido, empresa, edad, tipo} = this.state.cliente;
+
+                                const {emails} = this.state;
+
+                                if (nombre === '' || apellido === '' ||  empresa === '' || edad === '' || tipo === '') {
+                                    this.setState({
+                                        error: true
+                                    });
+                                    return;
+                                }
+
+                                this.setState({
+                                    error: false
+                                });
 
                                 const input = {
                                     nombre,
@@ -34,7 +82,7 @@ class NuevoCliente extends Component {
                                     empresa,
                                     edad: Number(edad),
                                     tipo,
-                                    email
+                                    emails
                                 };
 
                                 console.log(input)
@@ -78,7 +126,7 @@ class NuevoCliente extends Component {
                                 </div>
                             </div>
                             <div className="form-row">
-                                <div className="form-group col-md-6">
+                                <div className="form-group col-md-12">
                                     <label>Empresa</label>
                                     <input 
                                         type="text" 
@@ -94,21 +142,34 @@ class NuevoCliente extends Component {
                                         }}
                                     />
                                 </div>
-                                <div className="form-group col-md-6">
-                                    <label>Email</label>
-                                    <input 
-                                        type="email" 
-                                        className="form-control" 
-                                        placeholder="Email" 
-                                        onChange={e => {
-                                            this.setState({
-                                                cliente: {
-                                                    ...this.state.cliente,
-                                                    email: e.target.value
-                                                }
-                                            })
-                                        }}
-                                    />
+                                {this.state.emails.map((input, index) => (
+                                    <div key={index} className="form-group col-md-12">
+                                        <label>Correo {index + 1}: </label>
+                                        <div className="input-group">
+                                            <input
+                                                onChange={this.leerCampo(index)}
+                                                type="email"
+                                                placeholder="Email"
+                                                className="form-control"
+                                            />
+
+                                            <div className="input-group-append">
+                                                <button
+                                                    onClick={this.quitarCampo(index)}
+                                                    type="button"
+                                                    className="btn btn-danger"
+                                                >&times; Eliminar</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))}
+                                <div className="form-group d-flex justify-content-center col-md-12">
+                                        <button
+                                            onClick={this.nuevoCampo}
+                                            type="button"
+                                            className="btn btn-warning"
+                                        >+ Agregar Email
+                                        </button>
                                 </div>
                             </div>
                             <div className="form-row">
--- server/data/db.js
+++ server/data/db.js
@@ -8,7 +8,7 @@ const clientesSchema = new mongoose.Schema({
     nombre: String,
     apellido: String,
     empresa: String,
-    email: String,
+    emails: String,
     edad: Number,
     tipo: String,
     pedidos: Array
